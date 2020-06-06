a3f405164fec531d5475eaad2b4d6fef1f997955
Emeafu Francis Ebere <freemandigits@gmail.com>
2020-06-06 21:38:23 +0100

    active class complete

--- customestatic/js/scripts.js
+++ customestatic/js/scripts.js
@@ -7,15 +7,11 @@
 
     $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"#68ae00", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0'});
 
-	
-	
+
+
     $(".scrollbar1").getNiceScroll();
     if ($('body').hasClass('scrollbar1-collapsed')) {
         $(".scrollbar1").getNiceScroll().hide();
     }
 
 })(jQuery);
-
-                     
-     
-  
